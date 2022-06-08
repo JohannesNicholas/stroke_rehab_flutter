54ad92ae7c4b4c1bb8f94559f66c788d7933ac7b
Goanna7007 <45587025+Goanna7007@users.noreply.github.com>
2022-06-08 14:28:19 +1000

    Now shows all messages on records page

--- lib/record_page.dart
+++ lib/record_page.dart
@@ -10,11 +10,112 @@ class RecordPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final startDate = record.start?.toDate() ?? DateTime(0);
     return Scaffold(
       appBar: AppBar(
-        title: Text(record.title ?? "Untitled"),
+        title: Row(
+          children: [
+            Text(record.title ?? "Untitled"),
+            Text(
+              startDate.toString().substring(0, 19),
+              style: TextStyle(fontSize: 15),
+            )
+          ],
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        ),
       ),
-      body: Center(child: Text("Records page")),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: Center(child: Text("x repetitions in x.xxx seconds")),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: Center(child: Text("x correct presses")),
+          ),
+          Expanded(
+            child: ListView(
+              children: record.messages!.map((RecordMessage message) {
+                final time = message.datetime?.toDate() ?? DateTime(0);
+                final timeDiff =
+                    time.difference(startDate).inMilliseconds / 1000.0;
+                return Padding(
+                  padding: const EdgeInsets.fromLTRB(8, 8, 8, 16),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Text(
+                          message.message ?? "?",
+                          style: const TextStyle(fontSize: 18),
+                        ),
+                      ),
+                      Text(
+                        "+ " + timeDiff.toStringAsFixed(1) + " s",
+                        style: const TextStyle(
+                            color: Colors.orange,
+                            backgroundColor: Colors.black),
+                      ),
+                      Expanded(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Text(
+                              (message.correctPress == null)
+                                  ? ""
+                                  : message.correctPress!
+                                      ? "✅"
+                                      : "❌",
+                              style: const TextStyle(fontSize: 18),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  ),
+                );
+              }).toList(),
+            ),
+          )
+        ],
+      ),
+      floatingActionButton: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: FloatingActionButton.small(
+              onPressed: () {
+                //TODO
+              },
+              child: const Icon(Icons.camera),
+              backgroundColor: Colors.orange,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: FloatingActionButton.small(
+              onPressed: () {
+                //TODO
+              },
+              child: const Icon(Icons.delete_forever),
+              backgroundColor: Colors.orange,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: FloatingActionButton(
+              onPressed: () {
+                //TODO
+              },
+              child: const Icon(Icons.share),
+              backgroundColor: Colors.orange,
+            ),
+          ),
+        ],
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.end,
+      ),
     );
   }
 }
