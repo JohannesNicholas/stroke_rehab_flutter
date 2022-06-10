035c2e33b167e5522032808d1775a207c61970a9
Goanna7007 <45587025+Goanna7007@users.noreply.github.com>
2022-06-10 19:01:13 +1000

    Home page now ready

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:stroke_rehab/strings.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({
@@ -7,6 +8,114 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Home'));
+    return Center(
+        child: Padding(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 32),
+            child: SizedBox(
+              height: 180,
+              width: double.infinity,
+              child: Card(
+                child: Column(
+                  children: [
+                    const Center(
+                      child: Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Text('🏆 Exercise with Goals 🏆'),
+                      ),
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: ElevatedButton(
+                          onPressed: ((() {})),
+                          child: Text(
+                            Strings.normalGameTitle,
+                            style: TextStyle(color: Colors.black),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: ElevatedButton(
+                          onPressed: ((() {})),
+                          child: Text(
+                            Strings.sliderGameTitle,
+                            style: TextStyle(color: Colors.black),
+                          ),
+                          style: ButtonStyle(
+                            backgroundColor: MaterialStateProperty.all<Color>(
+                                Colors.orange[300] ?? Colors.orange),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 32),
+            child: SizedBox(
+              height: 180,
+              width: double.infinity,
+              child: Card(
+                child: Column(
+                  children: [
+                    const Center(
+                      child: Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Text('∞ Free-play ∞'),
+                      ),
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: ElevatedButton(
+                          onPressed: ((() {})),
+                          child: Text(
+                            Strings.normalGameTitle,
+                            style: TextStyle(color: Colors.black),
+                          ),
+                          style: ButtonStyle(
+                            backgroundColor: MaterialStateProperty.all<Color>(
+                                Colors.orange[300] ?? Colors.orange),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: ElevatedButton(
+                          onPressed: ((() {})),
+                          child: Text(
+                            Strings.sliderGameTitle,
+                            style: TextStyle(color: Colors.black),
+                          ),
+                          style: ButtonStyle(
+                            backgroundColor: MaterialStateProperty.all<Color>(
+                                Colors.orange[300] ?? Colors.orange),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                ),
+              ),
+            ),
+          ),
+        ],
+        mainAxisAlignment: MainAxisAlignment.start,
+      ),
+    ));
   }
 }
