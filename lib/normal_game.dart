2d2d37da52892b4db4e47d0afd73ffdf17e9fd5f
Goanna7007 <45587025+Goanna7007@users.noreply.github.com>
2022-06-11 00:52:37 +1000

    Random Buttons! WOOO!!!

--- lib/normal_game.dart
+++ lib/normal_game.dart
@@ -1,5 +1,5 @@
 import 'dart:async';
-
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -10,17 +10,19 @@ class NormalGame extends StatefulWidget {
   const NormalGame({Key? key}) : super(key: key);
 
   @override
-  State<NormalGame> createState() => _NormalGameState();
+  State<NormalGame> createState() => appear();
 }
 
-class _NormalGameState extends State<NormalGame> {
+class appear extends State<NormalGame> {
   List<NormalGameButtonData> buttons = [];
+
+  Random random = Random();
 
   var numberOfButtons = 5;
   var numberOfRounds = 5;
   var randomOrder = true;
   var highlightNextButton = true;
-  var buttonSize = 1;
+  var buttonSize = 4;
   var freePlay = false;
   String documentID = "";
   late Record recordData;
@@ -32,7 +34,10 @@ class _NormalGameState extends State<NormalGame> {
 
   @override
   Widget build(BuildContext context) {
-    resetButtons();
+    //if start of game,
+    if (buttons.isEmpty) {
+      resetButtons();
+    }
 
     return Scaffold(
       appBar: AppBar(
@@ -65,22 +70,31 @@ class _NormalGameState extends State<NormalGame> {
             child: Padding(
               padding: const EdgeInsets.all(16),
               child: Stack(
-                children: buttons.map((button) {
-                  return Positioned(
-                      top: button.posY,
-                      left: button.posX,
-                      height: buttonSize * 40,
-                      width: buttonSize * 40,
-                      child: FloatingActionButton(
-                        child: Text(
-                          button.number.toString(),
-                          style: TextStyle(fontSize: 30),
-                        ),
-                        onPressed: (() {}),
-                        backgroundColor: button.highlighted
-                            ? Colors.orange
-                            : Colors.orange[300],
-                      ));
+                children: getOrderedListOfButtons().toList().map((button) {
+                  return Column(
+                    children: [
+                      Spacer(flex: button.posY),
+                      Row(
+                        children: [
+                          Spacer(flex: button.posX),
+                          FloatingActionButton(
+                            child: Text(
+                              button.number.toString(),
+                              style: TextStyle(fontSize: 30),
+                            ),
+                            onPressed: (() {
+                              buttonPressed(button.number);
+                            }),
+                            backgroundColor: button.number == nextNumber
+                                ? Colors.orange
+                                : Colors.orange[300],
+                          ),
+                          Spacer(flex: 100 - button.posX)
+                        ],
+                      ),
+                      Spacer(flex: 100 - button.posY)
+                    ],
+                  );
                 }).toList(),
               ),
             ),
@@ -90,18 +104,44 @@ class _NormalGameState extends State<NormalGame> {
     );
   }
 
+//puts the highlighted button at the bottom of the stack, as to appear
+//at the top of the view, without anything else overlapping.
+  List<NormalGameButtonData> getOrderedListOfButtons() {
+    var newButtons = buttons.toList();
+    newButtons.sort((a, b) {
+      if (a.number == nextNumber) {
+        return 1;
+      }
+      if (b.number == nextNumber) {
+        return -1;
+      }
+      return 0;
+    });
+
+    return newButtons;
+  }
+
+  void buttonPressed(int button) {
+    print("Button $button pressed");
+    buttons[button - 1].highlighted = false;
+    buttons[button].highlighted = true;
+    nextNumber += 1;
+    setState(() {});
+  }
+
   void resetButtons() {
     buttons = [];
     for (var i = 0; i < numberOfButtons; i++) {
       buttons.add(NormalGameButtonData(
-          i.toDouble() * 30, i.toDouble() * 30, i + 1, false));
+          1 + random.nextInt(98), 1 + random.nextInt(98), i + 1, false));
     }
+    nextNumber = 1;
   }
 }
 
 class NormalGameButtonData {
-  double posX = 0;
-  double posY = 0;
+  int posX = 0;
+  int posY = 0;
   int number = 1;
   bool highlighted = false;
 
